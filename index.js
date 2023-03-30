678d7e215943c94318fac2d40d3aeda3d004e512
jcc <jccollart@outlook.be>
2023-03-30 22:09:24 +0200

    encours2

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 let aFaire;
 let array = [];
+array = window.localStorage.maboite.split(",");
+
+console.log(array);
+for (i = 0; i < array.length; i++) {
+  ul.innerHTML += "<li>" + array[i] + "</li>";
+}
+
 todo.addEventListener("input", (e) => {
   aFaire = e.target.value;
 });
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
+  //   e.preventDefault();
   array.push(aFaire);
-
-  let li = document.createElement("li");
+  window.localStorage.maboite = array;
   console.log(array);
-  let longueur = array.length;
-  ul.appendChild(li);
-  li.innerText = array[longueur - 1];
-  li.addEventListener("click", () => {
-    console.log("ok");
-  });
+});
+
+let fini;
+window.ul.addEventListener("click", (e) => {
+  if (fini == true) {
+    ul.removeChild(e.target);
+    fini = false;
+  } else {
+    let term = e.target.innerText;
+    e.target.innerHTML = term + "(supprimer)";
+    fini = true;
+  }
 });
