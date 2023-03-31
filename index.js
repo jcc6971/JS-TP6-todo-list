716c83bf976b22290e28522a1f17b303311e1b0b
jcc <jccollart@outlook.be>
2023-03-31 13:38:30 +0200

    encours3

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
 let aFaire;
 let array = [];
+let fini;
+let term;
+
+// transfert de localstorage vers le tableau
 array = window.localStorage.maboite.split(",");
 
-console.log(array);
+// affichage direct du tableau en list html
 for (i = 0; i < array.length; i++) {
   ul.innerHTML += "<li>" + array[i] + "</li>";
 }
+todo.focus();
 
+// valeur de l'input dans variable aFaire
 todo.addEventListener("input", (e) => {
   aFaire = e.target.value;
 });
+
+// variable aFaire vers tableau
 form.addEventListener("submit", (e) => {
-  //   e.preventDefault();
   array.push(aFaire);
   window.localStorage.maboite = array;
-  console.log(array);
 });
 
-let fini;
+//evenement au click dans list html
 window.ul.addEventListener("click", (e) => {
   if (fini == true) {
     ul.removeChild(e.target);
+    array.splice(array.indexOf(term), 1);
+    window.localStorage.maboite = array;
     fini = false;
   } else {
-    let term = e.target.innerText;
-    e.target.innerHTML = term + "(supprimer)";
+    e.target.classList.add("check");
     fini = true;
   }
 });
